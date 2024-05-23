039b9f24a8e3a90b12ea3c4b76f2302ef746b339
heldersoares97 <helderprofissional199@gmail.com>
2024-05-23 16:52:14 -0300

    feat: add endpoint get; doc: add endpoint get - LISTAR TODAS AS PUBLICACOES

--- api.hanami.blog/src/main/java/com/api/hanami/blog/service/PublicacaoService.java
+++ api.hanami.blog/src/main/java/com/api/hanami/blog/service/PublicacaoService.java
@@ -1,5 +1,7 @@
 package com.api.hanami.blog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +17,8 @@ public class PublicacaoService {
     public Publicacao save(Publicacao publicacao) {
         return publicacaoRepository.save(publicacao);
     }
+
+	public List<Publicacao> findAll() {
+		return publicacaoRepository.findAll();
+	}
 }
